62aa722bb5c37948e5396b347c6a88a77a2cc8cb
Zhogin Ivan <mr.i-zhogin@yandex.ru>
2022-12-27 16:08:18 +0300

    Filled main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,11 +3,17 @@ import java.util.Scanner
 fun main() {
     val userInput = Scanner(System.`in`)
     while (true) {
+        println("Введите число лайков: ")
         val likes = userInput.nextInt();
-        if (likes < 1) {
+        val exit = "exit"
+        if (likes < 1)
             println("Лайков нет")
-        } else if (likes.toString().endsWith("1"))
+        else if (likes.toString().endsWith("11"))
+            println("Понравилось $likes людям")
+        else if (likes.toString().endsWith("1"))
             println("Понравилось $likes человеку")
+        else if (likes.toString().equals(exit))
+            break
         else
             println("Понравилось $likes людям")
     }
